e104cf903d0887b487bfbe62073e213b98f5f405
Michele Cheow <ldnallen@gmail.com>
2021-02-20 12:21:09 -0800

    git init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import BookList from './BookList';
+import Search from './Search';
+import NewBookBtn from './NewBookBtn';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // Elements need to be nested, returns can only return one element. Wrapped inside of empty element called a fragment
+    <> 
+    <h1>Books</h1>
+      <Search />
+      <BookList />
+      <NewBookBtn />
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//const axios = require('axios');
+
+//console.log(resp.data);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
