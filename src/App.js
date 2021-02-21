1dd404e6fbfd5e87f3be5b2d01c19d6af146a99f
Michele Cheow <ldnallen@gmail.com>
2021-02-20 17:19:34 -0800

    Added node-sass, main layout components, styling for grid

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React from 'react';
 import BookList from './BookList';
 import Search from './Search';
 import NewBookBtn from './NewBookBtn';
+import './App.scss';
+
 
 function App() {
   return (
     // Elements need to be nested, returns can only return one element. Wrapped inside of empty element called a fragment
-    <> 
-    <h1>Books</h1>
-      <Search />
-      <BookList />
-      <NewBookBtn />
-    </>
+    <main className="container">
+      <div className="container__row"> 
+        <div className="container__col-xs-12">
+          <h1>Books</h1>
+        </div>
+        <Search />
+        <BookList />
+        <NewBookBtn />
+      </div>
+    </main>
   );
 }
 
--- src/NewBookBtn.js
+++ src/NewBookBtn.js
@@ -3,7 +3,28 @@ import React from 'react'
 export default function NewBookBtn() {
     return (
         <div>
-            
+            <button type="button" tabIndex="0">
+                Create New Book
+            </button>
+             {/*  TODO: figure out how to inject into new component and reference it here */} 
+            <div className="modal hidden">
+                <button className="close-modal">&times;</button>
+                <h2>Create New Book</h2>
+                <form>
+                    <label htmlFor="booktitle">Title:</label>
+                    <input type="text" name="booktitle" id="booktitle"/>
+
+                    <label htmlFor="bookauthor">Author:</label>
+                    <input type="text" name="bookauthor" id="bookauthor"/>
+
+                    <label htmlFor="bookpublisher">Publisher:</label>
+                    <input type="text" name="bookpublisher" id="bookpublisher"/>
+
+                    <label htmlFor="bookpublisheddate">Published Date:</label>
+                    <input type="text" name="bookpublisheddate" id="bookpublisheddate"/>
+                </form>
+            </div>
         </div>
+
     )
 }
