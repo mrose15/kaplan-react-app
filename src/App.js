2d35e937ec1f5700d7f3fcaffc24cb18189ab6ac
Michele Cheow <ldnallen@gmail.com>
2021-02-21 11:39:02 -0800

    add new reusable button component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import BookList from './Components/BookList';
-import Search from './Components/Search';
-import NewBookBtn from './Components/NewBookBtn';
+import BookList from './components/BookList';
+import Search from './components/Search';
+import Button from './components/Button';
 import './App.scss';
 
 function App() {
   return (
     <main className="container">
       <div className="container__row"> 
-        <div className="container__col-xs-8">
+        <div className="container__col-xs-6 container__col-md-8">
           <h1>Books</h1>
         </div>
-        <NewBookBtn />
+        <div className="container__col-xs-6 container__col-md-3 flex-align-center">
+          <Button variant="primary" size={"lg"}>Create New Book</Button>
+        </div>
         <Search />
         <BookList />
       </div>
---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+function Button(props){
+    const [size] = useState(props.size);
+    const [variant] = useState(props.variant);
+
+    return (
+        <button className={`btn btn--${variant} btn--${size}`}>{props.children}</button>
+    );
+}
+
+export default Button;
